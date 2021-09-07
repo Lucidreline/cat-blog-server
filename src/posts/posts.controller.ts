cf06bab4bbf0413f30dc83ec06cae4e0a15690b6
Lucidreline <casta.ma502@gmail.com>
2021-09-06 23:07:58 -0700

    Connected post controller with service.

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private postsService: PostsService) {}
+  @Get()
+  getPosts(): any {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  getPostById(@Param('id') id: string): any {
+    // TODO: have nest do auto parsing
+    return this.postsService.findById(Number(id));
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  private posts: any = [
+    {
+      id: 0,
+      imageUrl:
+        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
+      title: 'Denor',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
+      likeCounter: 1,
+    },
+    {
+      id: 1,
+      imageUrl:
+        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
+      title: 'Ipsum Dolor Sit',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
+      likeCounter: 5,
+    },
+    {
+      id: 2,
+      imageUrl:
+        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
+      title: 'Sit',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
+      likeCounter: 3,
+    },
+  ];
+
+  findAll() {
+    return this.posts;
+  }
+
+  findById(postId: number) {
+    return this.posts.find((post) => post.id === postId);
+  }
+}
