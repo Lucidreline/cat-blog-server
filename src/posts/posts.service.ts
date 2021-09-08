e2102c6fb7723a850d3294ff43600f20274e1f30
Lucidreline <casta.ma502@gmail.com>
2021-09-07 22:19:05 -0700

    Removed the in-memory array of posts.

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -21,36 +21,6 @@ export class PostsService {
     private blogPostModel: Model<BlogPostDocument>,
   ) {}
 
-  private posts: BlogPost[] = [
-    {
-      id: 0,
-      imageUrl:
-        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
-      title: 'Denor',
-      textBody:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
-      likeCounter: 1,
-    },
-    {
-      id: 1,
-      imageUrl:
-        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
-      title: 'Ipsum Dolor Sit',
-      textBody:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
-      likeCounter: 5,
-    },
-    {
-      id: 2,
-      imageUrl:
-        'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80',
-      title: 'Sit',
-      textBody:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, est laudantium necessitatibus.',
-      likeCounter: 3,
-    },
-  ];
-
   async findAll(query?: string): Promise<BlogPostModel[]> {
     const allBlogPosts = await this.blogPostModel.find();
 
