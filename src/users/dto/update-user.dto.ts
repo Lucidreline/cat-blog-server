ec1ac07e88f2399d491a7154e84dbc22419dabd5
Lucidreline <casta.ma502@gmail.com>
2021-09-09 02:18:10 -0700

    Planned out the methods in the users service.

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { BlogPost } from 'src/posts/schemas/post.schema';
+
+export class UpdateUserDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  blogPosts: BlogPost[];
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  registerUser(user: CreateUserDto) {}
+
+  loginUser(user: CreateUserDto) {}
+
+  updateUser(updateUser: UpdateUserDto) {}
+
+  deleteUser(userId: string): Promise<User> {}
+}
